e1dcef25969cae92ab25a998754a6f5c0616e8e7
Carlos Henrique <hencarlosdv@gmail.com>
2022-10-26 16:32:39 -0300

    docs: create documentation with swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
 const { Router } = require('express')
 const productController = require('../controllers/ProductController') 
 const usuarioController = require('../controllers/UsarioController')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
 const routes = Router();
 
-routes.get("/", async(req,res) => {
+routes.get("/test", async(req,res) => {
     res.send("test.")
 });
+
+routes.use('/api-docs',swaggerUi.serve)
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 //produtos
 routes.post("/cadastrarProduto", productController.cadastraProduto)
