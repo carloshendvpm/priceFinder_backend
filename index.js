3c94020e8b8fbf789c1467d309fff92ea60aa989
Carlos Henrique <hencarlosdv@gmail.com>
2022-11-16 00:18:39 -0300

    build: add bcrypt to auth

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-const routes = require('./src/routes')
-const cors = require('cors')
+const routes = require('./src/routes');
+const cors = require('cors');
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res,next) => {
     res.header("Acess-Control-Allow-Origin", "*")
     res.header("Acess-Control-Allow-Methods", "GET, PUT, POST DELETE")
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     app.use(cors())
     next();
 });
--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const Sequelize = require('sequelize');
 
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: 'localhost',
     dialect: 'mysql'
