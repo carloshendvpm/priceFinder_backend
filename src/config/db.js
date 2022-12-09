74d40d4b2d9cc834696a179482a19e8f759c164a
Carlos Henrique <hencarlosdv@gmail.com>
2022-12-09 06:22:56 -0300

    fix: changing db host

--- src/config/db.js
+++ src/config/db.js
@@ -33,7 +33,7 @@ const text = `
 });
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'postgres'
 });
 sequelize.authenticate()
